201407ec6701670682a2d58072a2b539eaf4a8a4
周易 <zhou.yi@99cloud.net>
2021-03-27 23:20:11 +0800

    feat: two sum java

---
+++ questions/leetcode/two-sum/Solution.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        Map<Integer, Integer> numMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (numMap.containsKey(nums[i])) {
+                return new int[]{numMap.get(nums[i]), i};
+            }
+            numMap.put(target - nums[i], i);
+        }
+
+        return null;
+    }
+}
