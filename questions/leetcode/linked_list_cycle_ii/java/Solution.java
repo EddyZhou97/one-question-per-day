99e35c97154d7a0a3a0fa276c3dc623477b8ad19
EddyZhou97 <401700108@qq.com>
2021-04-08 00:18:37 +0800

    feat: 链表是否有环,并返回进入环形的第一个节点,使用双指针方案进行解决

--- questions/leetcode/linked_list_cycle_ii/java/Solution.java
+++ questions/leetcode/linked_list_cycle_ii/java/Solution.java
@@ -25,7 +25,7 @@ class ListNode {
 }
 
 public class Solution {
-    public ListNode detectCycle(ListNode head) {
+    public ListNode detectCycleBak(ListNode head) {
         List<ListNode> nodes = new ArrayList<>();
         while(head != null){
             if(nodes.contains(head)){
@@ -39,15 +39,62 @@ public class Solution {
         return null;
     }
 
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head.next;
+        if(head != null && head.next != null){
+            ListNode fast = head.next.next;
+            while(fast != null && fast.next != null){
+                if(slow == fast){
+                    while(head != slow){
+                        head = head.next;
+                        slow = slow.next;
+                    }
+                    return head;
+                }
+                fast = fast.next.next;
+                slow = slow.next; 
+            }
+            return null;
+        }
+        return null;
+    }
+
     public static void main(String[] args) {
+        case1();
+        case2();
+    }
+
+    public static void case1(){
         ListNode node1 = new ListNode(1);
-        ListNode node2 = new ListNode(1);
-        ListNode node3 = new ListNode(1);
-        ListNode node4 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
         node1.next = node2;
         node2.next = node3;
         node3.next = node4;
         node4.next = node2;
-        System.out.println(new Solution().detectCycle(node1));
+        ListNode solution = new Solution().detectCycle(node1);
+        if(node2 == solution){
+            System.out.println("true");
+        } else {
+            System.out.println(solution);
+        }
+    }
+
+    public static void case2(){
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+
+        ListNode solution = new Solution().detectCycle(node1);
+        if(null == solution){
+            System.out.println("true");
+        } else {
+            System.out.println(solution);
+        }
     }
 }
