e543c109b4fa01c72a006fbae8f6c239b2c29af9
周易 <zhou.yi@99cloud.net>
2021-04-08 16:08:02 +0800

    feat: 添加leetcode全排列问题

---
+++ questions/leetcode/permutations/java/Solution.java
@@ -0,0 +1,41 @@
+package permutations.java;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution {
+
+    public static void case1(){
+        System.out.println(new Solution().permute(new int[]{1,2,3}));
+    }
+
+    public static void main(String[] args) {
+        case1();
+    }
+
+    public List<List<Integer>> permute(int[] nums){
+        List<List<Integer>> result = new LinkedList<>();
+        backtrack(nums, new LinkedList<>(), result);
+        return result;
+    }
+
+    public void backtrack(int[] nums, LinkedList<Integer> track, List<List<Integer>> result){
+        
+        if(track.size() == nums.length){
+            result.add(new LinkedList<>(track));
+            return;
+        }
+
+        for(int num : nums){
+            if(track.contains(num)){
+                continue;
+            }
+
+            track.add(num);
+            backtrack(nums, track, result);
+            track.removeLast();
+        }
+
+    }
+
+}
