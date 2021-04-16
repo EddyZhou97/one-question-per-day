58060132d399d3f41815b99d2a330ff63c91bedb
EddyZhou97 <401700108@qq.com>
2021-04-16 23:57:49 +0800

    feat: 添加长公共前缀题解

---
+++ questions/leetcode/longest_common_prefix/java/Solution.java
@@ -0,0 +1,60 @@
+package longest_common_prefix.java;
+
+public class Solution {
+
+    public String longestCommonPrefix(String[] strs) {
+
+        String commonStr = "";
+        if (strs.length > 0) {
+            commonStr = strs[0];
+            for (int i = 1; i < strs.length; i++) {
+                int length = Math.min(commonStr.length(), strs[i].length());
+                String tempStr = "";
+                for (int j = 0; j < length; j++) {
+                    if (commonStr.charAt(j) == strs[i].charAt(j)) {
+                        tempStr += commonStr.charAt(j);
+                    } else {
+                        break;
+                    }
+                }
+                commonStr = tempStr;
+            }
+        }
+        return commonStr;
+
+    }
+
+    public static void main(String[] args) {
+        case1();
+        System.out.println("=================================");
+        case2();
+        System.out.println("=================================");
+        case3();
+        System.out.println("=================================");
+        case4();
+        System.out.println("=================================");
+        case5();
+    }
+
+    private static void case1() {
+        System.out.println(new Solution().longestCommonPrefix(new String[]{"flower", "flow", "flight"}));
+    }
+
+    private static void case2() {
+        System.out.println(new Solution().longestCommonPrefix(new String[]{"dog", "racecar", "car"}));
+    }
+
+    private static void case3() {
+        System.out.println(new Solution().longestCommonPrefix(new String[]{"dog"}));
+    }
+
+    private static void case4() {
+        System.out.println(new Solution().longestCommonPrefix(new String[]{}));
+    }
+
+    private static void case5() {
+        System.out.println(new Solution().longestCommonPrefix(new String[]{"cir","car"}));
+    }
+
+
+}
