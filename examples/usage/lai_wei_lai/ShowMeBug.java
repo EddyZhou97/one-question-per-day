505ca1d2446564480fc0aed4e1dd621a9c0461c1
EddyZhou <401700108@qq.com>
2021-04-25 23:33:01 +0800

    feat: dfs遍历打印多叉树结构

--- examples/usage/lai_wei_lai/ShowMeBug.java
+++ examples/usage/lai_wei_lai/ShowMeBug.java
@@ -1,18 +1,30 @@
 package lai_wei_lai;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 // 必须定义 `ShowMeBug` 入口类和 `public static void main(String[] args)` 入口方法
 public class ShowMeBug {
+
+    static class TreeNode{
+        int preBlankCount;
+        Node current;
+        TreeNode parent;
+        List<TreeNode> children;
+
+        public TreeNode(int preBlankCount, Node current, TreeNode parent){
+            this.preBlankCount = preBlankCount;
+            this.current = current;
+            this.parent = parent;
+            this.children = new ArrayList<>();
+        }
+    }
+
     static class Node {
         int id;
         int parentId;
         String name;
 
-        public Node(int id, int parentId, String name) 				{
+        public Node(int id, int parentId, String name) {
             this.id = id;
             this.parentId = parentId;
             this.name = name;
@@ -22,15 +34,15 @@ public class ShowMeBug {
     public static void main(String[] args) {
         List<Node> nodeList = Arrays.asList(
                 new Node(1, 0, "AA"),
-                new Node(2, 1, "BB"), 
+                new Node(2, 1, "BB"),
                 new Node(3, 1, "CC"),
-                new Node(4, 3, "DD"), 
+                new Node(4, 3, "DD"),
                 new Node(5, 3, "EE"),
-                new Node(6, 2, "FF"), 
+                new Node(6, 2, "FF"),
                 new Node(7, 2, "GG"),
-                new Node(8, 4, "HH"), 
+                new Node(8, 4, "HH"),
                 new Node(9, 5, "II"),
-                new Node(10, 0, "JJ"), 
+                new Node(10, 0, "JJ"),
                 new Node(11, 10, "KK"),
                 new Node(12, 10, "LL"),
                 new Node(13, 12, "MM"),
@@ -41,31 +53,47 @@ public class ShowMeBug {
     }
 
     public static void print(List<Node> nodeList) {
-        //todo
-        List<List<Node>> res = new LinkedList<>();
-        List<List<Integer>> path = new LinkedList<>();
+        Map<Integer, TreeNode> treeNodeMap = new HashMap<>();
+        Node parentNode = new Node(0, 0, "");
+        TreeNode treeNode = new TreeNode(-2, parentNode, null);
+        treeNodeMap.put(0, treeNode);
         for(Node node : nodeList){
-
-            if(node.parentId == 0){
-                List<Integer> tmpPath = new LinkedList<>();
-                tmpPath.add(node.id);
-                path.add(tmpPath);
-                List<Node> nodeTree = new LinkedList<>();
-                nodeTree.add(node);
-                res.add(nodeTree);
-            } else {
-
-            }
-
+            final TreeNode parentTreeNode = treeNodeMap.get(node.parentId);
+            TreeNode curTreeNode = new TreeNode(parentTreeNode.preBlankCount + 2, node, parentTreeNode);
+            parentTreeNode.children.add(curTreeNode);
+            treeNodeMap.put(node.id, curTreeNode);
         }
 
+        for(TreeNode tmpTreeNode : treeNode.children){
+            traceTreeNode(tmpTreeNode);
+        }
+    }
+
+    public static void traceTreeNode(TreeNode treeNode){
+        System.out.println(getBlank(treeNode.preBlankCount) + treeNode.current.name);
+        if(treeNode.children.size() > 0){
+            for(TreeNode tmpTreeNode : treeNode.children){
+                traceTreeNode(tmpTreeNode);
+            }
+        }
+    }
+
+    public static String getBlank(int count){
+        if(count > 0){
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i<count;i++){
+                sb.append(" ");
+            }
+            return sb.toString();
+        }
+        return "";
     }
 }
 
 /*
-* AA
-*   BB
-*   CC
-*     DD
-*
-* */
+ * AA
+ *   BB
+ *   CC
+ *     DD
+ *
+ * */
