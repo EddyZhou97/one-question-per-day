12484410e678a9c2081d1edd5f6c113341314a50
EddyZhou97 <401700108@qq.com>
2021-05-10 23:19:24 +0800

    refactor: 添加 lc 0051 N皇后

---
+++ questions/leetcode/solution_0051/java/Solution.java
@@ -0,0 +1,22 @@
+package solution_0051.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 51. N 皇后
+ *
+ * @author ZhouYi
+ * 2021/5/10 11:09 下午
+ */
+public class Solution {
+
+    List<List<String>> result = new ArrayList<>();
+
+    public List<List<String>> solveNQueens(int n) {
+
+        return null;
+
+    }
+
+}
