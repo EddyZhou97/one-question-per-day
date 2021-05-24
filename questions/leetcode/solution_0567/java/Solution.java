0836270bd0f5dfcd5d5c60df0ed1f375facdf537
eddie-zhou-yi <401700108@qq.com>
2021-05-25 00:22:25 +0800

    feat: lc 0567 solution

--- questions/leetcode/solution_0567/java/Solution.java
+++ questions/leetcode/solution_0567/java/Solution.java
@@ -19,43 +19,76 @@ public class Solution {
      * @return
      */
     public boolean checkInclusion(String s1, String s2) {
-        if(s2.length() < s1.length()){
-            return  false;
+        if (s2.length() < s1.length()) {
+            return false;
         }
         Map<Character, Integer> strMap = new HashMap<>(s1.length());
+        int n = s1.length(), index = 0, diff = 0;
         for (char c : s1.toCharArray()) {
             Integer integer = strMap.get(c);
             if (integer == null) {
                 integer = 0;
             }
+            diff++;
             strMap.put(c, integer + 1);
         }
 
-        int n = s1.length(), index = 0;
-        while(index <= s2.length() - n){
-
-            for(int i = 0; i < n; i++){
-                char c = s2.charAt(index + i);
-                Integer integer = strMap.get(c);
-                if(integer != null){
-                    strMap.put(c, integer - 1);
+        for (int i = 0; i < n; i++) {
+            char c = s2.charAt(i);
+            Integer integer = strMap.get(c);
+            if (integer != null) {
+                integer = integer - 1;
+                strMap.put(c, integer);
+                if (integer >= 0) {
+                    diff--;
+                } else {
+                    diff++;
                 }
             }
-            if(strMap.values().stream().noneMatch(integer -> integer.compareTo(0) != 0)){
+        }
+        if (diff == 0) {
+            return true;
+        }
+        while (index + n < s2.length()) {
+
+            char headC = s2.charAt(index);
+            char tailC = s2.charAt(index + n);
+            if (headC == tailC) {
+                index ++;
+                continue;
+            } else {
+                Integer headCharCount = strMap.get(headC);
+                Integer tailCharCount = strMap.get(tailC);
+                if (headCharCount != null) {
+                    headCharCount = headCharCount + 1;
+                    strMap.put(headC, headCharCount);
+                    if (headCharCount > 0) {
+                        diff++;
+                    } else {
+                        diff--;
+                    }
+                }
+
+                if (tailCharCount != null) {
+                    tailCharCount--;
+                    strMap.put(tailC, tailCharCount);
+                    if (tailCharCount >= 0) {
+                        diff--;
+                    } else {
+                        diff++;
+                    }
+                }
+            }
+            if(diff == 0){
                 return true;
             }
-            final char c = s2.charAt(index);
-            Integer integer = strMap.get(c);
-            if(integer != null){
-                strMap.put(c, integer + 1);
-            }
-            index ++;
+            index++;
         }
-
         return false;
     }
 
     public static void main(String[] args) {
+        cases("adc", "dcda", true);
         cases("ab", "eidboaoo", false);
         cases("s1", "s2", false);
         cases("s22", "s22", true);
