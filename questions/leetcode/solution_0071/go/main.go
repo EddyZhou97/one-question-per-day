b44b4934dcf82a0019f7aafac17fee28e5731dd9
eddie-zhou-yi <401700108@qq.com>
2021-05-27 01:12:12 +0800

    feat: add lc 0071 go solution

---
+++ questions/leetcode/solution_0071/go/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func simplifyPath(path string) string {
+	splitPath := strings.Split(path, "/")
+	stack := make([]string, 0)
+	var res string
+	for i := 0; i < len(splitPath); i++ {
+		path := splitPath[i]
+		if path == ".." {
+			if len(stack) > 0 {
+				// 使用切片把元素从栈中推出
+				stack = stack[:len(stack)-1]
+			}
+		} else if path != "." && len(path) > 0 {
+			stack = append(stack, path)
+		}
+	}
+	if len(stack) == 0 {
+		return "/"
+	}
+	res = strings.Join(stack, "/")
+	return "/" + res
+}
+
+func main() {
+	cases("/../", "/")
+	cases("/home/", "/home")
+	cases("/home//foo/", "/home/foo")
+	cases("/a/./b/../../c/", "/c")
+}
+
+func cases(path string, expectedRes string) {
+	res := simplifyPath(path)
+	fmt.Println("input: " + path + "\noutput: " + res)
+	if res == expectedRes {
+		fmt.Println("AC")
+	} else {
+		fmt.Println("WA")
+	}
+}
