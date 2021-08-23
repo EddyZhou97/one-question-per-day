90b2fcea540c22e1b196d4af38e584e50944a315
eddie-zhou-yi <401700108@qq.com>
2021-08-23 09:33:37 +0800

    feat: add 0053 go solution use DP

---
+++ questions/leetcode/solution_0053/go/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func maxSubArray(nums []int) int {
+	max := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if nums[i]+nums[i-1] > nums[i] {
+			nums[i] += nums[i-1]
+		}
+		if nums[i] > max {
+			max = nums[i]
+		}
+	}
+	return max
+}
+
+func cases(nums []int, result int) {
+	computedRes := maxSubArray(nums)
+	if computedRes == result {
+		fmt.Println("AC")
+	} else {
+		fmt.Printf("WA\n, computed res: %v, ac res: %v \n", computedRes, result)
+	}
+}
+
+func main() {
+	cases([]int{0, 1}, 1)
+	cases([]int{-2, 1, 3, 4}, 8)
+	cases([]int{-3, 2, -2, 7, -1}, 7)
+}
