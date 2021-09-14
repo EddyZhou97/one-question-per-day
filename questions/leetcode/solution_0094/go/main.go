a72e12a9766f754606fcd12002cd7db137792a23
eddie-zhou-yi <401700108@qq.com>
2021-09-14 09:23:02 +0800

    feat: add 0094 go solution tag: 二叉树中序遍历 use: stack

---
+++ questions/leetcode/solution_0094/go/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "container/list"
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func inorderTraversal(root *TreeNode) []int {
+	res := []int{}
+	stack := list.New()
+	traceNode := root
+	for traceNode != nil || stack.Len() > 0 {
+		if traceNode != nil {
+			stack.PushBack(traceNode)
+			traceNode = traceNode.Left
+		} else {
+			e := stack.Back()
+			stack.Remove(e)
+			traceNode = e.Value.(*TreeNode)
+			res = append(res, traceNode.Val)
+			traceNode = traceNode.Right
+		}
+	}
+	return res
+}
+
+func main() {
+
+}
