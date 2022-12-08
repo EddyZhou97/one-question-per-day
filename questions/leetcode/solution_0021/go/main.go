ca3ed6f7b369869ae69155478544b1bca121164a
Richman <401700108@qq.com>
2022-12-08 23:02:47 +0800

    feat: add lc 0021 go solution

--- questions/leetcode/solution_0021/go/main.go
+++ questions/leetcode/solution_0021/go/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
@@ -7,6 +9,42 @@ type ListNode struct {
 
 func main() {
 
+	l1 := &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 4, Next: nil}}}
+	l2 := &ListNode{Val: 1, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4, Next: nil}}}
+
+	node := solution1(l1, l2)
+
+	for node != nil {
+		fmt.Print(node.Val)
+		fmt.Print(",")
+		node = node.Next
+	}
+
+}
+
+func solution1(l1 *ListNode, l2 *ListNode) *ListNode {
+	dummyNode := &ListNode{Val: -1}
+	p := dummyNode
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			p.Next = l1
+			l1 = l1.Next
+		} else {
+			p.Next = l2
+			l2 = l2.Next
+		}
+		p = p.Next
+	}
+
+	if l1 != nil {
+		p.Next = l1
+	}
+
+	if l2 != nil {
+		p.Next = l2
+	}
+
+	return dummyNode.Next
 }
 
 func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
