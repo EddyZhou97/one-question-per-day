4fc436301989d34f2ec92b04116b2b55f779a59f
Richman <401700108@qq.com>
2023-01-09 14:27:51 +0800

    feat: add ListNode common func

--- questions/leetcode/common/g/data_struct.go
+++ questions/leetcode/common/g/data_struct.go
@@ -6,3 +6,22 @@ type (
         Next *ListNode
     }
 )
+
+func FromArray(arr []int) *ListNode  {
+    dummyNode := &ListNode{Val: 0}
+    p := dummyNode
+    for _, e := range arr {
+        p.Next = &ListNode{Val: e}
+        p = p.Next
+    }
+    return dummyNode.Next
+}
+
+func ToArray(head *ListNode) []int {
+    res := []int{}
+    for head != nil {
+        res = append(res, head.Val)
+        head =head.Next
+    }
+    return res
+}
--- questions/leetcode/common/j/ListNode.java
+++ questions/leetcode/common/j/ListNode.java
@@ -1,5 +1,8 @@
 package common.j;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListNode {
     public int val;
     public ListNode next;
@@ -14,5 +17,24 @@ public class ListNode {
     public ListNode(int val, ListNode next) {
         this.val = val;
         this.next = next;
+    }
+
+    public static ListNode fromArrayList(List<Integer> arrays) {
+        ListNode dummyNode = new ListNode(0);
+        ListNode p = dummyNode;
+        for (Integer val : arrays) {
+            p.next = new ListNode(val);
+            p = p.next;
+        }
+        return dummyNode.next;
+    }
+
+    public static List<Integer> toArrayList(ListNode head) {
+        List<Integer> res = new ArrayList<>();
+        while (head != null) {
+            res.add(head.val);
+            head = head.next;
+        }
+        return res;
     }
 }
