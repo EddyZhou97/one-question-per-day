d6e2b1b3bb076e25b350857d3c4126c8206a4e94
Richman <401700108@qq.com>
2023-01-11 23:45:50 +0800

    feat: add lc 0092 solution

---
+++ questions/leetcode/solution_0092/Solution.md
@@ -0,0 +1,1 @@
+# (92. 反转链表 II)[https://leetcode.cn/problems/reverse-linked-list-ii/]
---
+++ questions/leetcode/solution_0092/java/Solution.java
@@ -0,0 +1,11 @@
+package solution_0092.java;
+
+import common.j.ListNode;
+
+public class Solution {
+
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        return null;
+    }
+
+}
