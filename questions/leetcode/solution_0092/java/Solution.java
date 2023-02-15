b93254ed69efdf395aa199c41a5d9a63f15d71cf
Richman <401700108@qq.com>
2023-02-15 18:13:08 +0800

    add lc 0092 solution

--- questions/leetcode/solution_0092/java/Solution.java
+++ questions/leetcode/solution_0092/java/Solution.java
@@ -5,9 +5,28 @@ import common.j.ListNode;
 public class Solution {
 
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        return null;
+        if (left == 1) {
+            return reverseN(head, right);
+        }
+        head.next = reverseBetween(head.next, left - 1, right - 1);
+        return head;
+    }
+
+    // 后驱节点
+    ListNode successor = null;
+
+    public ListNode reverseN(ListNode head, int n) {
+        if (n == 1) {
+            // 遍历到最后一个节点
+            successor = head.next;
+            return head;
+        }
+
+        ListNode last = reverseN(head.next, n - 1);
+        head.next.next = head;
+        head.next = successor;
+        return last;
     }
 
 }
-
 
